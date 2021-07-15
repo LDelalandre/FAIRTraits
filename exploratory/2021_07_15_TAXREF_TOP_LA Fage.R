8e7fb51095b7df3b4f11a6e2a830bf4c1b5c736d
Leo DELALANDRE <delalandre@newcefe.newage.fr>
2021-07-15 16:44:31 +0200

    More traits

--- exploratory/2021_07_15_TAXREF_TOP_LA Fage.R
+++ exploratory/2021_07_15_TAXREF_TOP_LA Fage.R
@@ -5,24 +5,35 @@ source("functions/TAXREF and TOP.R")
 
 # I) Import datasets ####
 # I.1) TaXREF and TOP
-taxref <- read.csv2("data/TAXREF/TAXREF14.0_FR_Continental_13_07_2021.csv")
+taxref <- read.csv2("data/TAXREF/TAXREF14.0_FR_Continental_13_07_2021.csv") # long to charge
 TOP <- read.csv2("data/TOP/TOP_info.csv",fill=T)
 
 # I.2) Traits
-leafmorpho <-  read.table("data/La Fage/LeafMorpho_traits.txt",header=T,sep="\t") %>% 
+LeafMorpho <-  read.table("data/La Fage/LeafMorpho_traits.txt",header=T,sep="\t") %>% 
   mutate(occurrenceID = paste("FAG",Treatment,Day,Code_Sp,"1",sep = "_")) # 1 = group of traits 1 (i.e. leafmorpho)
-CN <- read.table("data/La Fage/LeafC&N.txt",header=T,sep="\t") %>% 
-  mutate(occurrenceID = paste("FAG",Treatment,Day,Code_Sp,"2",sep = "_"))
-
+LeafDimensions <- read.table("data/La Fage/LeafDimensions.txt",header=T,sep="\t") %>% 
+  mutate(occurrenceID = paste("FAG",Treatment,Day,Code_Sp,"2",sep = "_")) # 1 = group of traits 1 (i.e. leafmorpho)
+LeafCN <- read.table("data/La Fage/LeafC&N.txt",header=T,sep="\t") %>% 
+  mutate(occurrenceID = paste("FAG",Treatment,Day,Code_Sp,"3",sep = "_"))
+LeafP <- read.table("data/La Fage/LeafP.txt",header=T,sep="\t") %>% 
+  mutate(occurrenceID = paste("FAG",Treatment,Day,Code_Sp,"4",sep = "_"))
+Biovolume <- read.table("data/La Fage/Biovolume.txt",header=T,sep="\t") %>% 
+  mutate(occurrenceID = paste("FAG",Treatment,Day,Code_Sp,"5",sep = "_"))
+Seed <- read.table("data/La Fage/Seed.txt",header=T,sep="\t") %>% 
+  mutate(occurrenceID = paste("FAG",Treatment,Day,Code_Sp,"6",sep = "_"))
 
 
 # II) TAXREF info ####
-TRAITS <- list(c("SLA","LDMC","L_Area"),
-               c("LNC","LCC")) # list of traits for each group of traits
-GROUPS_OF_TRAITS <- list(leafmorpho,CN) # datasets
-groups_of_traits <- c("leafmorpho","CN") # names of the datasets
+TRAITS <- list(c("SLA","LDMC","L_Area"), # list of traits for each group of traits
+               c("L_Area"),
+               c("LNC","LCC"),
+               c("LPC"),
+               c("Hveg","Hrepro"),
+               c("SeedMass"))
+GROUPS_OF_TRAITS <- list(LeafMorpho,LeafDimensions,LeafCN,LeafP,Biovolume,Seed) # datasets
+groups_of_traits <- c("LeafMorpho","LeafDimensions","LeafCN","LeafP","Biovolume","Seed") # names of the datasets
 
-for (i in c(1,2)){ # index of the trait sheet used
+for (i in 1:length(groups_of_traits)){ # index of the trait sheet used
   group_of_traits <- groups_of_traits[i]
   focus <- GROUPS_OF_TRAITS[[i]]
   
@@ -46,7 +57,7 @@ for (i in c(1,2)){ # index of the trait sheet used
 
 # III) Tidy and TOP info ####
 TIDY <- NULL
-for (i in c(1,2)){ # index of the trait sheet used
+for (i in 1:length(groups_of_traits)){ # index of the trait sheet used
   group_of_traits <- groups_of_traits[i]
   focus <- GROUPS_OF_TRAITS[[i]]
   
@@ -59,17 +70,18 @@ for (i in c(1,2)){ # index of the trait sheet used
     focus_trait <- focus_name_id %>% 
       select(Species,trait,scientificName,taxonID,occurrenceID) %>%
       rename(verbatimScientificName = Species) %>% 
-      filter(verbatimScientificName=="Bromus erectus") %>% #/!\ just for a test with a subset of the data
-      head() %>%
+      # filter(verbatimScientificName=="Bromus erectus") %>% #/!\ just for a test with a subset of the data
+      # head() %>%
       gather(trait,key=verbatimTraitName,value=verbatimTraitValue)
     # Automate verbatimTraitUnit for each trait (get it from the metadata)
     
     # III.2) Add Standard trait name from TOP
-    colsWanted <- c("traitName","TOPLocalIdentifier","traitID","verbatimTraitUnit")
+    colsWanted <- c("traitName","LocalIdentifier","traitID","verbatimTraitUnit")
     for(columnWanted in colsWanted){
-      leafmorpho_trait <- focus_trait %>%  mutate(!!columnWanted:= map2_chr(trait,columnWanted,get_info_TOP,TOP))
+      focus_trait <- focus_trait %>%  mutate(!!columnWanted:= map2_chr(trait,columnWanted,get_info_TOP,TOP))
     }
-    TIDY <- rbind(TIDY,leafmorpho_trait)
+    TIDY <- rbind(TIDY,focus_trait)
   }
 }
+write.csv2(TIDY,"data/La Fage/processed/III) Traitdata_test.csv",row.names=F)
   
