c069992009e76e19b70fa76b7c36244fbe88439b
Delalandre <leo.delalandre@cefe.cnrs>
2023-02-13 12:20:08 -0300

    scripts Léo

--- scripts/generate_data_in_line.R
+++ scripts/generate_data_in_line.R
@@ -1,30 +1,26 @@
 library(tidyverse)
 library("openxlsx")
 
-
 # functions
-source("scripts/functions/TAXREF and TOP.R")
 source("scripts/1_read_files.R")
-
-
 
 
 #_______________________________________________________________________________
 # Tidy ####
-# generate data frame in the good format for the data paper
+# generate data frame in the good format for the data paper (= lines)
 columns_other_than_traits <- 
   c("Site", "Block" ,"Plot", "Treatment", "Year"     ,   "Day"       ,       "Species" , "Code_Sp"   ,    "Family" ,
     "LifeForm1" ,"LifeForm2" , 
     "Rep",
     "nameOfProject" ,"measurementDeterminedBy","verbatimOccurrenceID")
 
-    # columns to add in the species file
+# columns to add in the species file
 new_columns_sp_file <- c(    "plantType",	"leafType")
 
-    #columns for gas exchange:
+#columns for gas exchange:
 columns_gas_exchange_metadata <- c(  "dataType"  , "instrumentOutputStatus", 
                                      "leafStatus" , "leafAreaBasis", "leafAreaMethod"  ,
-    "plantAge" , "leafAge", "canopyPosition" ,"lightExposure" , "timeOfDay"   )
+                                     "plantAge" , "leafAge", "canopyPosition" ,"lightExposure" , "timeOfDay"   )
 # /!\ these columns have to be added in the methods, linked to measurementOrFact,  and associated with
 # verbatimOccurrenceID
 
@@ -44,13 +40,13 @@ for (site in fsites){ # site
   
   for (i in c(1:length(files))){ # excel sheet
     if (length(files))
-    focus <- files[[i]]
+      focus <- files[[i]]
     
     focus_traits <- focus %>%
       select(!all_of(columns_other_than_traits)) %>%
       colnames()  # extract the name of the traits available on that sheet
-
-      
+    
+    
     # which are the colmuns that are missing? 
     missing_plant_leaf_type <- setdiff(new_columns_sp_file,colnames(focus))
     missing_metadata <- setdiff(columns_gas_exchange_metadata,colnames(focus))
@@ -66,7 +62,7 @@ for (site in fsites){ # site
         # some columns, i.e. those in new_columns_sp_file and incolumns_gas_exchange_metadata, are not there
         gather(all_of(trait) ,key=verbatimTraitName,value=verbatimTraitValue)
       focus_trait$Day <- as.character(focus_trait$Day)
-
+      
       TIDY <- rbind(TIDY,focus_trait)
     }
     
@@ -109,68 +105,3 @@ TIDY2 <- TIDY %>%
   ))
 
 write.csv2(TIDY2,"output/ETS_format_Bar_Caz_Gar.csv",row.names=F)
-
-
-#_______________________________________________________________________________
-# TOP ####
-TOP <- read.csv2("data/traits/list of traits_jan2023_part_per_thousand.csv",fill=T,fileEncoding="latin1")
-TIDY <- read.csv2("output/ETS_format_Bar_Caz_Gar.csv")
-
-# focus_trait_TOP <- merge(focus_trait,TOP,by="verbatimTraitName") %>% 
-#   select(all_of(colnames(TIDY)),everything())
-
-TIDY_traits <- merge(TIDY,TOP,by="verbatimTraitName") %>% 
-  select(all_of(colnames(TIDY)),everything()) %>% 
-  relocate("verbatimTraitValue", .after = last_col()) %>% 
-  relocate("verbatimTraitUnit", .after = last_col())%>% 
-  relocate("measurementMethod", .after = last_col())
-
-#_______________________________________________________________________________
-# TaXREF and TOP
-# taxref <- read.csv2("data/TAXREF/TAXREF14.0_FR_Continental_13_07_2021.csv") # long to charge
-taxref <- read.table("data/TAXREF_v16/TAXREFv16.txt",header=T,sep = "\t")
-
-
-# TAXREF ####
-# list_sp <- read.csv2("output/Species_Code_Sp.csv")
-list_sp <- TIDY %>% 
-  select(Species,Code_Sp) %>% 
-  unique()
-
-
-
-list_sp_scientificName <- list_sp %>% 
-  mutate(scientificName = map_chr(Species,get_scientificName,taxref)) 
-
-list_sp_name_URL <- list_sp_scientificName %>% 
-  mutate(taxonID = map_chr(Species,get_CD_NOM,taxref))
-  # mutate(scientificName = if_else(Species == "Myosostis ramosissima subsp. ramosissima","Myosotis ramosissima Rochel, 1814 subsp. ramosissima",scientificName)) %>% 
-  # mutate(taxonID = if_else(Species == "Myosostis ramosissima subsp. ramosissima","https://inpn.mnhn.fr/espece/cd_nom/137934",taxonID))
-
-list_sp_name_URL_id <- list_sp_name_id %>% 
-  mutate(taxonURL = map_chr(Species,get_URL,taxref)) 
-
-write.csv2(list_sp_name_URL_id,"output/list of species_TAXREF_Bar_Caz_Gar.csv",row.names=F)
-
-# Add taxref info ####
-# NB: in fine, I will add all the info from the species data frame (LifeForm1, etc.), 
-# with the info from the floras, etc.
-
-TIDY_traits_TAXREF <- TIDY_traits %>% 
-  merge(list_sp_name_URL_id,by=c("Species","Code_Sp")) %>% # add TAXREF info
-  select(Species,scientificName,taxonID,taxonURL,everything(),verbatimTraitValue,verbatimTraitUnit) %>% 
-  rename(verbatimScientificName = Species)
-
-write.csv2(TIDY_traits_TAXREF,"output/ETS_format_Bar_Caz_Gar_traits_TAXREF.csv",row.names=F,fileEncoding="latin1")
-
-
-#_______________________________________________________________________________
-# List of all the traits ####
-TIDY <- read.csv2("output/Traitdata_test.csv")
-
-list_of_traits <- TIDY %>% 
-  pull(verbatimTraitName) %>% 
-  unique() 
-
-df_traits <- data.frame(verbatimTraitName = list_of_traits)
-write.csv2(df_traits,"output/list of traits.csv",row.names=F)
